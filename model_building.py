95c42c04b762e7bb546174276a1984026fce7e50
yazanziyad98 <yazanziyad980@gmail.com>
2021-12-07 23:39:31 +0200

    Update model_building.py

--- model_building.py
+++ model_building.py
@@ -14,20 +14,19 @@ df2 = df.copy()
  
 
 df.columns
-
+df['MPAA Rating'].value_counts()
 # get dummy data 
 Rating_encoded = pd.get_dummies(df2['MPAA Rating'])
 df_final=df2.join(Rating_encoded, rsuffix='1')
 
 
 abs(df_final.corr()['Rating']).sort_values(ascending=False)
-
-
+ 
 label =df_final.Rating
 
 
 #drp columns that are mostly zeros
-df_final.drop(['NC-17','TV-14','Unrated','TV-PGA'],axis=1,inplace=True)
+df_final.drop(['Gross'],axis=1,inplace=True)
 
 # drop categorical columns and encoded column
 df_final.drop(['Director','Title','MPAA Rating','Rating'],axis=1,inplace=True)
@@ -56,11 +55,10 @@ from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 
-
-
+ 
 lm = LinearRegression()
 lm.fit(normalized_train,y_train)
-scores = cross_val_score(lm,X_train,y_train, scoring = 'neg_mean_squared_error', cv= 35)
+scores = cross_val_score(lm,X_train,y_train, scoring = 'neg_mean_squared_error', cv= 95)
 rmse_scores = np.sqrt(-scores)
 rmse = np.mean(rmse_scores)
 print(rmse)
@@ -74,7 +72,7 @@ for i in range(1,100):
     alpha.append(i/100)
     lml = Lasso(alpha=(i/100))
  
-    scores = cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_squared_error', cv= 35)
+    scores = cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_squared_error', cv= 95)
     rmse_scores = np.sqrt(-scores)
     rmse = np.mean(rmse_scores)
     error.append(rmse)
@@ -82,20 +80,21 @@ for i in range(1,100):
 plt.plot(alpha,error) 
 
 from sklearn.ensemble import RandomForestRegressor
-
-regr = RandomForestRegressor(max_features=15, n_estimators=45, random_state=100)
-regr.fit(normalized_train, y_train)
-scores = cross_val_score(regr, normalized_train, y_train,scoring="neg_mean_squared_error", cv=38)
-tree_rmse_scores = np.sqrt(-scores)
-rmse = np.mean(tree_rmse_scores)
-print(rmse)
+for i in range(40,75,35):
+    print(i)
+    regr = RandomForestRegressor(n_estimators=45, random_state=100)
+    regr.fit(normalized_train, y_train)
+    scores = cross_val_score(regr, normalized_train, y_train,scoring="neg_mean_squared_error", cv=95)
+    tree_rmse_scores = np.sqrt(-scores)
+    rmse = np.mean(tree_rmse_scores)
+    print(rmse)
 
 
 from sklearn.model_selection import GridSearchCV
 
 parameters = {'n_estimators':range(10,300,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
 
-grid_search = GridSearchCV(regr,parameters,scoring='neg_mean_absolute_error',cv=8)
+grid_search = GridSearchCV(regr,parameters,scoring='neg_mean_absolute_error',cv=12)
 grid_search.fit(X_train,y_train)
 grid_search.best_score_
 grid_search.best_estimator_
@@ -114,6 +113,5 @@ with open(file_name, 'rb') as pickled:
     model = data['model']
 
 model.predict(np.array(list(normalized_test[0])).reshape(1,-1))[0]
-
 list(normalized_test[0])
  
